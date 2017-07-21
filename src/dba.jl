5c2d66b0a57e7a71d33de7735b6228492c0ce98a
holgerteichgraeber <holger@teichgr.de>
2017-07-20 21:47:08 -0700

    _sequentize updated for v0.6. Did not support untyped generator

--- src/dba.jl
+++ src/dba.jl
@@ -144,14 +144,14 @@ function dba( s::AbstractArray, args...; kwargs... )
     dba(_sequentize(s), args...; kwargs...)
 end
 
-#=
-# julia v0.6 gives error message LoadError: generated function body is not pure. this likely means it contains a closure or comprehension.
+
 @generated function _sequentize{T,N}(s::AbstractArray{T,N})
-    :( [ Sequence(@ncall($N, view, s, n-> n==$N ? i : Colon())) for i = 1:size(s,2) ] )
+    :( Sequence[ Sequence(@ncall($N, view, s, n-> n==$N ? i : Colon())) for i = 1:size(s,2) ] )
 end
-=#
+
 
 # This function replaces the compile-time function which doesn't work in v0.6
-function _sequentize(s::AbstractArray)
-    [Sequence(s[:,i]) for i=1:size(s,2)]
-end
+# Compile-time function fixed, generated functions to not support untyped generators
+#function _sequentize(s::AbstractArray)
+#    [Sequence(s[:,i]) for i=1:size(s,2)]
+#end
